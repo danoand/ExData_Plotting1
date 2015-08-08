a58fe10beacb3e3b0d0bdbbcf126fca2e8ba1d2f
Dan Anderson <danandersonmobile@yahoo.com>
2015-08-07 20:34:46 -0500

    update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,13 +5,13 @@
 ####
 
 # Read the massaged data to be plotted
-source("readdata/dumpdata.R")
+source("dumpdata.R")
 
 # Configure a png device
-png(filename="figure/plot1.png", width=480, height=480)
+png(filename="plot1.png", width=480, height=480)
 
 # Create a histogram object to be plotted to a png file
-hist_obj <- hist(myFrame.sub$Global_active_power, bg="clear", col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+hist_obj <- hist(myFrame.sub$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
 
 # Plot the histogram object and save to a png file
 plot(hist_obj)
